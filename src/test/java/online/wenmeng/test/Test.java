661c692fdb25fcd6aa881a5a55469f826e19f563
lwj <15518064076@163.com>
2020-12-06 23:33:55 +0800

    Initial commit

---
+++ src/test/java/online/wenmeng/test/Test.java
@@ -0,0 +1,11 @@
+package online.wenmeng.test;
+
+/**
+ * @Auther: 狼芒
+ * @Date: 2020/12/6 9:58
+ * @Descrintion:
+ * @version: 1.0
+ */
+public class 
+Test {
+}
---
+++ src/test/java/online/wenmeng/test/Util.java
@@ -0,0 +1,10 @@
+package online.wenmeng.test;
+
+/**
+ * @Auther: 狼芒
+ * @Date: 2020/12/6 9:57
+ * @Descrintion:
+ * @version: 1.0
+ */
+public class Util {
+}
