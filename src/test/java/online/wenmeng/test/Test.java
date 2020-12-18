e76866cc093410d9c8275005b5c003d9891c1435
lwj <15518064076@163.com>
2020-12-19 00:39:18 +0800

    修改相应文档，并添加SQL文件，使之变为一个优雅的SSM项目

--- src/test/java/online/wenmeng/test/Test.java
+++ src/test/java/online/wenmeng/test/Test.java
@@ -1,11 +1,28 @@
 package online.wenmeng.test;
 
+import online.wenmeng.controler.UserControler;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * @Auther: 狼芒
  * @Date: 2020/12/6 9:58
  * @Descrintion:
  * @version: 1.0
  */
-public class 
-Test {
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext*")
+public class Test {
+
+    @Autowired
+    private UserControler userControler;
+
+
+    public void fun(){
+        String user = userControler.getUser(1);
+        System.out.println(user);
+    }
 }
